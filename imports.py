762e6d262c4d24b1d571e97e234bd2dfbc20cd8d
Carlos Ordonez <44660718+Carlos-Ordonez@users.noreply.github.com>
2019-01-30 14:08:17 -0500

    Add files via upload

---
+++ imports.py
@@ -0,0 +1,13 @@
+import sys
+import numpy
+import scipy
+import sklearn
+import matplotlib
+import pandas
+
+print(sys.version)
+print(numpy.__version__)
+print(scipy.__version__)
+print(sklearn.__version__)
+print(matplotlib.__version__)
+print(pandas.__version__)
